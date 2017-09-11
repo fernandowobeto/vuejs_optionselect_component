5149b60c6088bcbf671904c586a00779932fc010
vagrant <fernandowobeto@gmail.com>
2017-09-11 16:09:37 +0000

    melhorado componente

--- js.js
+++ js.js
@@ -1,42 +1,42 @@
 new Vue({
-   el: 'body',
-   data: {
-      disabled: true,
-      dados: []
-   },
-   methods: {
-      enableDisable: function(){
-         this.disabled = !this.disabled;
-      },
-     getSelected: function(){
-
-     }
-   },
-  ready: function(){
-      this.data = [{
-        id: 1,
-        nome: 'Rio Grande do Sul'
-      }, {
-        id: 2,
-        nome: 'Rio Grande do Norte'
-      }, {
-        id: 3,
-        nome: 'Paraná'
-      }, {
-        id: 4,
-        nome: 'Santa Catarina'
-      }, {
-        id: 5,
-        nome: 'São Paulo'
-      }, {
-        id: 6,
-        nome: 'Rio de Janeiro'
-      }, {
-        id: 7,
-        nome: 'Manaus'
-      }, {
-        id: 8,
-        nome: 'Acre'
-      }];
+  el: 'body',
+  data: {
+    disabled: true,
+    dados: []
+  },
+  methods: {
+    enableDisable: function () {
+      this.disabled = !this.disabled;
+    },
+    getSelected: function () {
+     console.log(this.$refs.option.getSelected());
+    }
+  },
+  ready: function () {
+    this.dados = [{
+      id: 1,
+      nome: 'Rio Grande do Sul'
+    }, {
+      id: 2,
+      nome: 'Rio Grande do Norte'
+    }, {
+      id: 3,
+      nome: 'Paraná'
+    }, {
+      id: 4,
+      nome: 'Santa Catarina'
+    }, {
+      id: 5,
+      nome: 'São Paulo'
+    }, {
+      id: 6,
+      nome: 'Rio de Janeiro'
+    }, {
+      id: 7,
+      nome: 'Manaus'
+    }, {
+      id: 8,
+      nome: 'Acre'
+    }];
   }
 });
--- optionselect.vuejs.js
+++ optionselect.vuejs.js
@@ -1,5 +1,5 @@
 Vue.component('dual-list', {
-   template: `<div class="row">
+  template: `<div class="row">
     <div class="col-xs-5">
       <select class="form-control" style="height: 135px" v-model="selectedLeft" multiple :disabled="disable">
       	<option v-for="disponivel in disponiveis" :value="disponivel" @dblclick="goRight(disponivel)">{{getDescription(disponivel)}}</option>	
@@ -19,91 +19,93 @@ Vue.component('dual-list', {
       </select>
     </div>
   </div>`,
-   props: {
-      data: {
-         type: Array,
-         required: true
-      },
-      disable: {
-         type: Boolean,
-         default: false
-      },
-      description: {
-         type: String,
-         default: 'description'
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    disable: {
+      type: Boolean,
+      default: false
+    },
+    description: {
+      type: String,
+      default: 'description'
+    }
+  },
+  data: function () {
+    return {
+      selectedLeft: [],
+      selectedRight: [],
+    }
+  },
+  methods: {
+    getDescription: function (item) {
+      return item[this.description];
+    },
+    rightOne: function () {
+      if (!this.disable) {
+        this.selectedLeft.forEach(function (selected) {
+          Vue.set(selected, 'utilizado', true);
+        });
+        this.selectedLeft = [];
       }
-   },
-   data: function() {
-      return {
-         selectedLeft: [],
-         selectedRight: [],
+    },
+    rightAll: function () {
+      if (!this.disable) {
+        this.data.forEach(function (dado) {
+          Vue.set(dado, 'utilizado', true);
+        });
       }
-   },
-   methods: {
-      getDescription: function(item){
-         return item[this.description];
-      },
-      rightOne: function() {
-         if(!this.disable){
-            this.selectedLeft.forEach(function(selected) {
-               selected.utilizado = true;
-            });
-            this.selectedLeft = [];
-         }
-      },
-      rightAll: function() {
-         if(!this.disable){
-            this.data.forEach(function(dado) {
-               dado.utilizado = true;
-            });
-         }
-      },
-      leftOne: function() {
-         if(!this.disable){
-            this.selectedRight.forEach(function(selected) {
-               selected.utilizado = false;
-            });
-            this.selectedRight = [];
-         }
-      },
-      leftAll: function() {
-         if(!this.disable){
-            this.data.forEach(function(dado) {
-               dado.utilizado = false;
-            });
-         }
-      },
-      goRight: function(a, b) {
-         if(!this.disable){
-            a.utilizado = true;
-         }
-      },
-      goLeft: function(a, b) {
-         if(!this.disable){
-            a.utilizado = false;
-         }
+    },
+    leftOne: function () {
+      if (!this.disable) {
+        this.selectedRight.forEach(function (selected) {
+          selected.utilizado = false;
+        });
+        this.selectedRight = [];
       }
-   },
-   computed: {
-      disponiveis: function() {
-         var self = this;
-         return this.dados.filter(function(el) {
-            return el.utilizado == false;
-         }).sort(function(a, b) {
-            if (a[self.description] < b[self.description]) return -1;
-            if (a[self.description] > b[self.description]) return 1;
-            return 0;
-         });
-      },
-      utilizados: function() {
-         var self = this;
-         return this.dados.filter(function(el) {
-            return el.utilizado == true;
-         }).sort(function(a, b) {
-            if (a[self.description] < b[self.description]) return -1;
-            if (a[self.description] > b[self.description]) return 1;
-            return 0;
-         });
+    },
+    leftAll: function () {
+      if (!this.disable) {
+        this.data.forEach(function (dado) {
+          dado.utilizado = false;
+        });
       }
-   }
+    },
+    goRight: function (a) {
+      if (!this.disable) {
+        Vue.set(a, 'utilizado', true);
+      }
+    },
+    goLeft: function (a) {
+      if (!this.disable) {
+        a.utilizado = false;
+      }
+    },
+    getSelected: function () {
+      return this.data.filter(function (el) {
+        return el.utilizado === true;
+      });
+    },
+    getAvailable: function () {
+      return this.data.filter(function (el) {
+        return el.utilizado === false;
+      });
+    }
+  },
+  computed: {
+    disponiveis: function () {
+      var self = this;
+      return this.data.filter(function (el) {
+        return el.utilizado === false || typeof el.utilizado == 'undefined';
+      });
+    },
+    utilizados: function () {
+      var self = this;
+      return this.data.filter(function (el) {
+        return el.utilizado === true;
+      });
+    }
+  }
 });
